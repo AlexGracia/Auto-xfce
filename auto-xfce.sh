9c588562f56d54a798d2d8fcec40854cc0124ddb
AlexGracia <6606113+AlexGracia@users.noreply.github.com>
2025-03-12 15:40:55 +0100

    refactor: modifica /dev/null

--- auto-xfce.sh
+++ auto-xfce.sh
@@ -49,7 +49,7 @@ f_comprobaciones_iniciales () {
 
     # Comprobar la red.
     echo "Comprobando la conexion de red ..."
-    ping 1.1.1.1 -c 1 -s 1 -q >/dev/null 2>1&
+    ping 1.1.1.1 -c 1 -s 1 -q >/dev/null 2>&1
     if [ $? != 0 ]; then
         f_error "Debes tener conexion de red."
     fi
@@ -177,9 +177,9 @@ f_onlyoffice () {
 f_firefox () {
     # Añadir repositorio.
     install -d -m 0755 /etc/apt/keyrings
-    wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+    wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | tee /etc/apt/keyrings/packages.mozilla.org.asc >/dev/null
     gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
-    echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+    echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | tee -a /etc/apt/sources.list.d/mozilla.list >/dev/null
     echo '
 Package: *
 Pin: origin packages.mozilla.org
