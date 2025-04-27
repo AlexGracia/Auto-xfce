cd7861d307b9b9017f02434b79eead4d11c35c04
AlexGracia <6606113+AlexGracia@users.noreply.github.com>
2025-04-27 15:45:15 +0200

    feat: añade personalizacion

--- auto-xfce.sh
+++ auto-xfce.sh
@@ -533,21 +533,101 @@ f_configurar_hidden () {
 f_personalizar_xfce () {
     f_titulo "Personalizando XFCE      " 15
 
-#    Para cambiar el tema de XFCE, puedes utilizar el comando xfconf-query:
+    # Variables
+    estilo=""
+    letra="Serif Bold 18"
+    tema=""
+    tipografia="Serif Bold 26"
+    cursor=""
+    tamanio_cursor="48"
+    carpeta_iconos="/home/$usuario/.icons"
+    carpeta_tema="/home/$usuario/.themes"
 
-#    xfconf-query -c xsettings -p /Net/ThemeName -s nombre_del_tema
+    # Carpeta temporal
+    cd /tmp/
+    mkdir auto-xfce
+    cd auto-xfce/
+
+    # Descargas
+    # Color carpetas
+    wget -q https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-folders/refs/heads/master/papirus-folders
+    # Cursor
+    wget -qO cursores.zip https://github.com/vinceliuice/Fluent-icon-theme/archive/refs/heads/master.zip
+    # Tema
+    wget -qO temas.zip https://github.com/AlexGracia/Temas-xfwm4/archive/refs/heads/master.zip
+
+    # Carpetas creadas
+    # Iconos
+    mkdir $carpeta_iconos
+    # Tema
+    mkdir $carpeta_tema
+
+    # Descomprimir
+    # Cursor
+    7z x cursores.zip '-xr!.gitignore' '-xr!.github' '-xr!colors' '-xr!links' '-xr!release' '-xr!src' '-xr!templates' '-xr!AUTHORS' '-xr!COPYING' '-xr!fluent-icon.jpg' '-xr!install.sh' '-xr!README.md' '-xr!build.sh' '-xr!LICENSE' '-xr!logo.png' '-xr!logo.svg' '-xr!preview-01.png' '-xr!preview-02.png'
+    # Tema
+    7z x temas.zip '-xr!img' '-xr!*.md'
+
+    if [ $personalizacion = "f" ]; then
+        estilo="HighContrast"
+        # Tema
+        tema="Gris-light"
+        cp -r "Temas-xfwm4-master/$tema" "$carpeta_tema"
+
+        # Cursor
+        cursor="Fluent-cursors"
+        cp -r Fluent-icon-theme-master/cursors/dist/ "$carpeta_iconos/$cursor"
+
+        # Cambiar color de carpeta
+        papirus-folders -C paleorange --theme Papirus-Light
+
+        # Posicionar panel abajo
+        xfconf-query -c xfce4-panel -p /panels/panel-1/position -s 'p=8;x=0;y=0'
+    else
+        estilo="Adwaita-dark"
+
+        # Tema
+        tema="Oliva-dark"
+        cp -r "Temas-xfwm4-master/$tema" "$carpeta_tema"
+
+        # Cursor
+        cursor="Fluent-dark-cursors"
+        cp -r Fluent-icon-theme-master/cursors/dist/ "$carpeta_iconos/$cursor"
+
+        # Cambiar color de carpeta
+        papirus-folders -C paleorange --theme Papirus-Dark
+
+        # Posicionar panel arriba
+        xfconf-query -c xfce4-panel -p /panels/panel-1/position -s 'p=6;x=0;y=0'
+    fi
+
+    # Apariencia
+    xfconf-query -c xsettings -p /Net/ThemeName -s $estilo
+    xfconf-query -c xsettings -p /Gtk/FontName -s $letra
+
+    # Gestor de ventanas
+    xfconf-query -c xfwm4 -p /general/theme -s $tema
+    xfconf-query -c xfwm4 -p /general/title_font -s $tipografia
+
+    # Cursor
+    xfconf-query -c xsettings -p /Gtk/CursorThemeName -s $cursor
+    xfconf-query -c xsettings -p /Gtk/CursorThemeSize -s $tamanio_cursor
+
+    # Panel
+    # Estilo, color sólido
+    xfconf-query -c xfce4-panel -p /panels/panel-1/background-style -s 1
+    # Color de fondo, negro
+    xfconf-query -c xfce4-panel -p /panels/panel-1/background-rgba -t double -s 0.000000 -t double -s 0.000000 -t double -s 0.000000 -t double -s 1.000000
+    # Tamaño de la fila, 52 px
+    xfconf-query -c xfce4-panel -p /panels/panel-1/size -s 52
+    # Tamaño de los iconos, 16 px
+    xfconf-query -c xfce4-panel -p /panels/panel-1/icon-size -s 16
+    # Mostrar el indicador del modo de presentación
+    xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/show-presentation-indicator -s true
 
 #    Para cambiar el fondo de pantalla, puedes utilizar el comando xfconf-query:
 
 #    xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -s /ruta/al/fondo/de/pantalla
-
-#    Para cambiar la configuración de la barra de tareas, puedes utilizar el comando xfconf-query:
-
-#    xfconf-query -c xfce4-panel -p /panels/panel-1/config -s configuración_de_la_barra
-
-#    Para cambiar la configuración del escritorio, puedes utilizar el comando xfconf-query:
-
-#    xfconf-query -c xfce4-desktop -p /desktop/config -s configuración_del_escritorio
 
 # Recargar escritorio.
 #xfdesktop --reload
