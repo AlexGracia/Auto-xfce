30ab02e18b692994b961bc201c45b17272b32a33
AlexGracia <6606113+AlexGracia@users.noreply.github.com>
2025-05-21 15:14:19 +0200

    style: ordena paquetes

--- auto-xfce.sh
+++ auto-xfce.sh
@@ -9,8 +9,8 @@
 # Variables globales
 desatendido=$1
 personalizacion=$1
-paquetes_frecuentes="anacron evince galculator mousepad network-manager network-manager-gnome photoflare p7zip-full redshift redshift-gtk gthumb sakura sudo thunar-archive-plugin ufw vlc xfce4 xfce4-power-manager xfce4-whiskermenu-plugin zram-tools cups printer-driver-all simple-scan"
-paquetes_infrecuentes="chromium evince firejail gimp gnome-boxes gnumeric gpicview network-manager network-manager-gnome p7zip-full pandoc qpdf redshift redshift-gtk sakura sd sudo ufw vlc xfce4 xfce4-power-manager zram-tools"
+paquetes_frecuentes="anacron cups evince galculator gthumb mousepad network-manager network-manager-gnome p7zip-full photoflare printer-driver-all redshift redshift-gtk sakura simple-scan sudo thunar-archive-plugin ufw vlc xfce4 xfce4-power-manager xfce4-whiskermenu-plugin zram-tools"
+paquetes_infrecuentes="chromium evince firefox firejail gimp git gnome-boxes gnumeric gpdf gpicview jigdo network-manager network-manager-gnome optipng p7zip-full pandoc qpdf redshift redshift-gtk sakura sd sudo ufw vlc xfce4 xfce4-power-manager zram-tools"
 usuario=""
 
 # Funcion para mostrar un titulo descriptivo del paso actual.
