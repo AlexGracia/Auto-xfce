65a8ce6dfad7bd0b56b8461e1c4ac39c7503727b
AlexGracia <6606113+AlexGracia@users.noreply.github.com>
2025-06-25 13:38:27 +0200

    docs: modifica version

--- auto-xfce.sh
+++ auto-xfce.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Descripcion: Script que instala Xfce mínimo, paquetes, configuraciones y personalizaciones.
 # Autor: Alex Gracia
-# Version: 0.15.4
+# Version: 0.16.0
 # Requisitos: paqueteria APT, conexion de red, usuario root y paquete wget
 # URL: https://github.com/AlexGracia/Auto-xfce
 #════════════════════════════════════════
