53b79becd788fdf4030731361bd6fee72e6a3f0f
AlexGracia <6606113+AlexGracia@users.noreply.github.com>
2025-08-27 19:08:30 +0200

    feat: añade comprobar actualizaciones

---
+++ scripts-secundarios/comprobar-actualizaciones.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+# Descripcion: Script que comprueba la disponibilidad de actualizaciones.
+# Autor: Alex Gracia
+# Version: 0.1.0
+#════════════════════════════════════════
+
+# Funcion para avisar de que hay actualizaciones disponibles.
+_actualizacion_disponible () {
+    export DISPLAY=:0.0
+    # Obtener nombre de usuario.
+    usuario=$(getent group users | cut -d: -f4 -s | sed -n 1p)
+    if [ "$usuario" = "" ]; then
+        usuario=$(getent passwd | grep home | cut -d: -f1 -s | sed -n 1p)
+        if [ "$usuario" = "" ]; then
+            usuario=$(cat /etc/passwd | grep home | cut -d: -f1 -s | sed -n 1p)
+            if [ "$usuario" = "" ]; then
+                echo
+                echo "Usuario no encontrado."
+                exit 1
+            fi
+        fi
+    fi
+    export XAUTHORITY="/home/$usuario/.Xauthority"
+    # Mostrar aviso.
+    xmessage -center -title "Actualización disponible" -buttons "" "Actualice el PC." -fn 12x24
+}
+
+# Comprobar actualizaciones.
+apt update | grep upgradable
+
+# Avisar.
+if [ $? = 0 ]; then
+    _actualizacion_disponible
+fi
+
+exit
